9553db1a0f8a37c27ad0de6c7906c558719d41f1
kalle <kmthorsnes@gmail.com>
2018-03-06 13:42:16 +0100

    edit app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  data = {};
+  private apiUrl = 
+  'https://ca-adress-book.herukuapp.com/api/contacts';
+
+    constructor(private http: Http) {
+      this.getContacts().subscribe(data => {
+        console.log(data);
+      });
+    }
+    
+    getContacts() {
+      return this.http.get(this.apiUrl)
+        .map((res: Response) => res.json());
+    }
 }
